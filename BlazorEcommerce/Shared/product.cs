a53e7120b49799e657c9d40320d1b4eaa7a41164
chama <chama@DESKTOP-6VJNE2H>
2024-02-18 14:16:56 +0530

    Add project files.

---
+++ BlazorEcommerce/Shared/product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{
+	public class product
+	{
+		public int id { get; set; }
+		public string Title { get; set; } = string.Empty;//Set String empty as default
+		public string Description { get; set; } = string.Empty;
+
+		public string ImageUrl { get; set; }
+
+		public decimal price { get; set; }
+	}
+}
